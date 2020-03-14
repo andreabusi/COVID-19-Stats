78e8d4e627ee301d185963793b1d755678353a1b
Andrea Busi <busi.andrea@gmail.com>
2020-03-14 19:20:32 +0100

    Initial Commit

---
+++ COVID-19 Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID-19 Stats
+//
+//  Created by Busi Andrea on 14/03/2020.
+//  Copyright © 2020 BubiDevs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
